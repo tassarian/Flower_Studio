83b11f40511458d0239bc3526d99aeccede5215c
victoriasyluma <victoriasyluma3@gmail.com>
2023-02-21 23:41:37 +0100

    animation fixes

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,47 +3,57 @@
  * this is the neccesary javascript for the mobile menu
  */
 (() => {
-    const maxSize = 768; // pixels
-    const isOpenClass = 'is_open';
-  
-    const modalId = 'mobile_menu';
-    const openMobileMenuId = 'open_mobile_menu';
-    const closeMobileMenuId = 'close_mobile_menu';
-    const navigationLinkMobileClass = 'navigations__link_mb';
-  
-    const mobileMenu = document.getElementById(modalId);
-    const openMobileMenu = document.getElementById(openMobileMenuId);
-    const closeMobileMenu = document.getElementById(closeMobileMenuId);
-    const navigationLinks = Array.from(
-      document.getElementsByClassName(navigationLinkMobileClass)
-    );
-  
-    const closeMenu = () => {
-      mobileMenu.classList.remove(isOpenClass);
-    };
-  
-    const openMenu = () => {
-      mobileMenu.classList.add(isOpenClass);
-    };
-  
-    /**
-     * Validate if the window size is greater than the max size
-     * if it is, close the menu
-     */
-    const onResize = () => {
-      const isMaxSizeReached = window.innerWidth > maxSize;
-  
-      if (!isMaxSizeReached) return;
-  
-      closeMenu();
-    };
-  
-    openMobileMenu.addEventListener('click', openMenu);
-    closeMobileMenu.addEventListener('click', closeMenu);
-  
-    navigationLinks.forEach(link => {
-      link.addEventListener('click', closeMenu);
-    });
-  
-    window.addEventListener('resize', onResize);
-  })();
+  const maxSize = 768; // pixels
+  const isOpenClass = 'is_open';
+
+  const modalId = 'mobile_menu';
+  const openMobileMenuId = 'open_mobile_menu';
+  const closeMobileMenuId = 'close_mobile_menu';
+  const navigationLinkMobileClass = 'navigations__link_mb';
+
+  const headerAndHeroContainer = document.getElementById(
+    'header_and_hero_container'
+  );
+  const mobileMenu = document.getElementById(modalId);
+  const openMobileMenu = document.getElementById(openMobileMenuId);
+  const closeMobileMenu = document.getElementById(closeMobileMenuId);
+  const navigationLinks = Array.from(
+    document.getElementsByClassName(navigationLinkMobileClass)
+  );
+
+  const closeMenu = () => {
+    mobileMenu.classList.remove(isOpenClass);
+  };
+
+  const openMenu = () => {
+    mobileMenu.classList.add(isOpenClass);
+  };
+
+  /**
+   * Validate if the window size is greater than the max size
+   * if it is, close the menu
+   */
+  const onResize = () => {
+    const isMaxSizeReached = window.innerWidth > maxSize;
+
+    if (!isMaxSizeReached) return;
+
+    closeMenu();
+  };
+
+  openMobileMenu.addEventListener('click', openMenu);
+  closeMobileMenu.addEventListener('click', closeMenu);
+
+  navigationLinks.forEach(link => {
+    link.addEventListener('click', closeMenu);
+  });
+
+  window.addEventListener('resize', onResize);
+
+  headerAndHeroContainer.addEventListener('mouseover', () => {
+    headerAndHeroContainer.classList.add('header_and_hero_container');
+
+    // remove the event listener
+    headerAndHeroContainer.removeEventListener('hover');
+  });
+})();
